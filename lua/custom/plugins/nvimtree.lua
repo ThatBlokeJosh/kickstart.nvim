df2606c13295ea9de13b370cb10e3ec705f17b7a
Your Name <you@example.com>
2023-12-12 20:15:57 +0100

    Window switching fix

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -3,6 +3,7 @@ return {
   version = "*",
   lazy = true,
   vim.keymap.set('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>'),
+
   vim.keymap.set('n', '<space>e', '<Cmd>wincmd w<CR>'),
   dependencies = {
     "nvim-tree/nvim-web-devicons",
